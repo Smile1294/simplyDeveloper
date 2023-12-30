c475539b988146f05029d17c186c1ef11e91f287
david <david@eachthing.com>
2023-12-30 18:02:15 +0100

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-full">
+    <div class="w-full mb-24">
         <h1 class="text-center mt-4 text-5xl font-bold">Welcome</h1>
         <p class="text-center mt-2 text-2xl">This is my personal website, where I share my thoughts and ideas.</p>
         <p class="text-center mt-2 text-2xl">I am a software developer, and I love to learn new things.</p>
